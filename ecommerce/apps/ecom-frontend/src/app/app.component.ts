9d8e01eeb9420b3ef9025efefff3f8183990e2fd
“[jacob-majesty]” <[jacobamony@gmail.com]>
2025-01-05 19:11:20 -0300

    chore(frontend): Add footer and navbar components

---
+++ ecommerce/apps/ecom-frontend/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FaConfig, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { NavbarComponent } from "./layout/navbar/navbar.component";
+import { FooterComponent } from "./layout/footer/footer.component";
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { NgClass } from '@angular/common';
+
+@Component({
+  standalone: true,
+  imports: [
+    RouterModule,
+    FaIconComponent,
+    NavbarComponent,
+    FooterComponent,
+    NgClass,
+],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent implements OnInit {
+
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+  title = 'ecom-frontend';
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+}
